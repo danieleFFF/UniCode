4d688fd3cd620429924659b85059c56dcf210fa4
Daniele Filice <59928344+danieleFFF@users.noreply.github.com>
2025-10-28 15:01:42 +0100

    Creazione progetto UniCode.

---
+++ src/main/java/it/unical/unicode/UniCodeApplication.java
@@ -0,0 +1,13 @@
+package it.unical.unicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/unicode/UniCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.unicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
