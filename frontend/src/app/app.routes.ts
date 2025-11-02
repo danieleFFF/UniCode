068a46157b1f34fe4125b0216229780b4453ba09
Samu <ssamu3909@gmail.com>
2025-11-02 19:52:13 +0100

    Setup:Header,Home and the basic routing of the pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+
+];
