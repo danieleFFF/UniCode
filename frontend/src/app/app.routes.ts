a23d62f5ac4ace3d60ff84378316bab164e2487d
Samu <ssamu3909@gmail.com>
2025-11-04 17:48:58 +0100

    redirect navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,18 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Login } from './login/login';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: Login,
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./login/login').then(m => m.Login)
   },
   {
-    path: 'home',
-    component: Home
+    path: '',
+    loadComponent: () => import('./home/home').then(m => m.Home),
+    pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   }
 
 ];
