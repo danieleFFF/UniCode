9da462c88760eec70521b0079fd51caf36e7d244
Samu <ssamu3909@gmail.com>
2025-11-05 12:34:41 +0100

    Small updates, created info-cards component

---
+++ frontend/src/app/home/info-cards/info-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-cards',
+  imports: [],
+  templateUrl: './info-cards.html',
+  styleUrl: './info-cards.scss',
+})
+export class InfoCards {
+
+}
