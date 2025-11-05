ecd790c0139d54193a7cc288cf9edce04da87366
Samu <ssamu3909@gmail.com>
2025-11-05 16:37:46 +0100

    Small updates, created info-cards component

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../layout/navbar/navbar';
 import { Hero } from './hero/hero';
 import { Courses} from './courses/courses';
+import {InfoCards} from './info-cards/info-cards';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero,Courses],
+  imports: [Navbar, Hero,Courses,InfoCards],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
