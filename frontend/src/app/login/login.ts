1b0974edb1f901c40b06421129bd7fa94bb8a06c
Daniele Filice <59928344+danieleFFF@users.noreply.github.com>
2025-11-05 15:20:11 +0100

    Frontend Login

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import { RouterLink } from '@angular/router';
+=======
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Location } from '@angular/common';
+import {RouterLink} from '@angular/router';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-login',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterLink],
+=======
+  imports: [CommonModule, FormsModule, RouterLink],
+>>>>>>> Stashed changes
   templateUrl: './login.html',
 })
+<<<<<<< Updated upstream
 export class Login {}
+=======
+export class Login {
+  email: string= '';
+  password: string= '';
+  showPassword: boolean = false;
+  constructor(private location: Location) {}
+  goBack(){
+    this.location.back();
+  }
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  //Disabilita la possibilità di copiare la password con Ctrl+C.
+  disableCopy(event: ClipboardEvent) {
+    event.preventDefault();
+  }
+
+}
+>>>>>>> Stashed changes
