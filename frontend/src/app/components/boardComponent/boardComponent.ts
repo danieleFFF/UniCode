fce222fbcf4566d7964db660b385e692a0bd05b3
eug <grecoe02@gmail.com>
2025-11-11 11:25:09 +0100

    new new new !!!

---
+++ frontend/src/app/components/boardComponent/boardComponent.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-board',
+    templateUrl: './boardComponent.html',
+    styleUrls: ['./boardComponent.scss']
+  }
+)
+
+export class BoardComponent {}
