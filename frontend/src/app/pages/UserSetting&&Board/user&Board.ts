710ab88567cf121725b492a65cfff9608fcc4c08
eug <grecoe02@gmail.com>
2025-11-12 16:49:08 +0100

    work work work !!!!!

--- frontend/src/app/pages/UserSetting&&Board/user&Board.ts
+++ frontend/src/app/pages/UserSetting&&Board/user&Board.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {UserSettings} from '../../components/userSettings/userSettings';
 import {Navbar} from '../../layout/navbar/navbar';
+import {BoardComponent} from '../../components/boardComponent/boardComponent';
 
 @Component(
   {
     selector: 'app-userBoard',
-    imports: [UserSettings, Navbar],
+    imports: [UserSettings, Navbar , BoardComponent],
     templateUrl: './user&Board.html',
-    styleUrls: ['./user&Board.scss','../../components/userSettings/userSettings.scss','../../layout/navbar/navbar.scss']
+    styleUrls: ['./user&Board.scss','../../components/userSettings/userSettings.scss','../../layout/navbar/navbar.scss',
+      '../home/home.scss','../../components/boardComponent/boardComponent.scss']
   }
 )
 
