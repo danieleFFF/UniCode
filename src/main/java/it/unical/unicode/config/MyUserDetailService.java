3343a1c3ba10b46446b9622aee88b0831fa70907
eug <grecoe02@gmail.com>
2025-12-13 17:19:46 +0100

    moved PasswordEncoder bean to ApplicationConfig

--- src/main/java/it/unical/unicode/config/MyUserDetailService.java
+++ src/main/java/it/unical/unicode/config/MyUserDetailService.java
@@ -2,12 +2,10 @@ package it.unical.unicode.config;
 
 import it.unical.unicode.dao.UserDAO;
 import it.unical.unicode.model.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -21,10 +19,6 @@ public class MyUserDetailService implements UserDetailsService {
         this.userDAO = userDAO;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
