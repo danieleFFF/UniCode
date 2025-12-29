5240cbba5597a800b04f316d6f758ffdf10d1e57
Daniele Filice <59928344+danieleFFF@users.noreply.github.com>
2025-12-29 15:49:53 +0100

    users added

---
+++ src/main/resources/db/migration/V14__populate_users.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.users (username, email, password_hash, total_points, id_avatar) VALUES
+('Marco_Dev', 'marco.rossi@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 240, 1),
+('Sara_Code', 'sara.bianchi@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 180, 2),
+('LucaJS', 'luca.ferrari@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 310, 5),
+('GiuliaP', 'giulia.moretti@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 980, 4),
+('Alex_Python', 'alessandro.greco@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 420, 1),
+('Chiara_01', 'chiara.romano@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 150, 6),
+('Matteo_Cpp', 'matteo.colombo@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 280, 3),
+('Elena_Dev', 'elena.ricci@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 70, 4),
+('DavideX', 'davide.marino@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 190, 7),
+('Francesca_98', 'francesca.galli@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 340, 2),
+('SimoneJS', 'simone.conti@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 80, 3),
+('ValentinaC', 'valentina.costa@email.com', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZRGdjGj/n3PY.d0BwRE6vpIFkY4m2', 210, 8);
