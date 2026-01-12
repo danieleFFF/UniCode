dcc498beb384bcbefbb8b90f2cdaba27388ed148
Daniele Filice <59928344+danieleFFF@users.noreply.github.com>
2026-01-12 16:04:48 +0100

    fix + more exercises

---
+++ src/main/resources/db/migration/V19__fix_html_exercise_points.sql
@@ -0,0 +1,3 @@
+UPDATE exercises SET points = 10 WHERE id_language = 4 AND difficulty = 'Easy';
+UPDATE exercises SET points = 50 WHERE id_language = 4 AND difficulty = 'Medium';
+UPDATE exercises SET points = 100 WHERE id_language = 4 AND difficulty = 'Hard';
